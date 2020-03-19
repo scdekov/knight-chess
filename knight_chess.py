1cd5e0b62e6a82dd049ca7e0f036bc09ac0808e3
svetoslav dekov <svetoslav.dekov@mentormate.com>
2020-03-19 14:14:21 +0200

    Implement chess move method

---
+++ knight_chess.py
@@ -0,0 +1,16 @@
+class KnightChess:
+    MOVE_ACTIONS = {
+        1: (1, 2),
+        2: (2, 1),
+        3: (2, -1),
+        4: (1, -2),
+        5: (-1, -2),
+        6: (-2, -1),
+        7: (-2, 1),
+        8: (-1, 2)
+    }
+
+    @classmethod
+    def move(cls, starting_point, move_type):
+        action = cls.MOVE_ACTIONS[move_type]
+        return (starting_point[0] + action[0], starting_point[1] + action[1])
