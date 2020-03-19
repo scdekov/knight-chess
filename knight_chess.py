bc64cba0ce47735be4d56f7abda5b1bc5dc2120b
svetoslav dekov <svetoslav.dekov@mentormate.com>
2020-03-19 14:55:14 +0200

    Implement solve method

--- knight_chess.py
+++ knight_chess.py
@@ -3,21 +3,44 @@ import math
 
 class KnightChess:
     MOVE_ACTIONS = {
-        1: (1, 2),
-        2: (2, 1),
-        3: (2, -1),
-        4: (1, -2),
-        5: (-1, -2),
-        6: (-2, -1),
-        7: (-2, 1),
-        8: (-1, 2)
+        '1': (1, 2),
+        '2': (2, 1),
+        '3': (2, -1),
+        '4': (1, -2),
+        '5': (-1, -2),
+        '6': (-2, -1),
+        '7': (-2, 1),
+        '8': (-1, 2)
     }
+
+    @classmethod
+    def solve(cls, moves):
+        furthest_point = (0, 0)
+        furthest_distance = 0
+        same_x_y_points = set()
+
+        position = (0, 0)
+        for move in moves:
+            position = cls._move(position, move)
+            if position[0] == position[1]:
+                same_x_y_points.add(position)
+
+            distance_from_start = cls._distance_from_start(position)
+            if distance_from_start > furthest_distance:
+                furthest_point = position
+                furthest_distance = distance_from_start
+
+        return {
+            'end_position': position,
+            'furthest_point': furthest_point,
+            'same_x_y_points': list(same_x_y_points)
+        }
 
     @classmethod
     def _move(cls, starting_point, move_type):
         action = cls.MOVE_ACTIONS[move_type]
         return (starting_point[0] + action[0], starting_point[1] + action[1])
 
-    @classmethod
-    def _distance_from_start(cls, point):
+    @staticmethod
+    def _distance_from_start(point):
         return math.sqrt(point[0] ** 2 + point[1] ** 2)
--- tests.py
+++ tests.py
@@ -2,20 +2,41 @@ from knight_chess import KnightChess
 
 
 class TestKnightChess:
+    def test_solve(self):
+        assert KnightChess.solve('123') == {
+            'end_position': (5, 2),
+            'furthest_point': (5, 2),
+            'same_x_y_points': [(3, 3)]
+        }
+
+    def test_solve__no_moves(self):
+        assert KnightChess.solve('') == {
+            'end_position': (0, 0),
+            'furthest_point': (0, 0),
+            'same_x_y_points': []
+        }
+
+    def test_solve__multiple_same_x_y_points(self):
+        assert KnightChess.solve('121237') == {
+            'end_position': (6, 6),
+            'furthest_point': (8, 5),
+            'same_x_y_points': [(3, 3), (6, 6)]
+        }
+
     def test_move(self):
-        assert KnightChess._move((0, 0), 1) == (1, 2)
-        assert KnightChess._move((1, 2), 1) == (2, 4)
-        assert KnightChess._move((2, 4), 1) == (3, 6)
+        assert KnightChess._move((0, 0), '1') == (1, 2)
+        assert KnightChess._move((1, 2), '1') == (2, 4)
+        assert KnightChess._move((2, 4), '1') == (3, 6)
 
-        assert KnightChess._move((0, 0), 1) == (1, 2)
-        assert KnightChess._move((1, 2), 2) == (3, 3)
-        assert KnightChess._move((3, 3), 3) == (5, 2)
+        assert KnightChess._move((0, 0), '1') == (1, 2)
+        assert KnightChess._move((1, 2), '2') == (3, 3)
+        assert KnightChess._move((3, 3), '3') == (5, 2)
 
-        assert KnightChess._move((0, 0), 1) == (1, 2)
-        assert KnightChess._move((1, 2), 5) == (0, 0)
+        assert KnightChess._move((0, 0), '1') == (1, 2)
+        assert KnightChess._move((1, 2), '5') == (0, 0)
 
-        assert KnightChess._move((0, 0), 5) == (-1, -2)
-        assert KnightChess._move((-1, -2), 6) == (-3, -3)
+        assert KnightChess._move((0, 0), '5') == (-1, -2)
+        assert KnightChess._move((-1, -2), '6') == (-3, -3)
 
     def test_distance_from_start(self):
         assert KnightChess._distance_from_start((3, 4)) == 5
